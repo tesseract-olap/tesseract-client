8176edf374a8b235678cd2e4511e3ebcfc73dbb6
Francisco Abarzúa <git.id@frabarz.cl>
2019-05-15 17:23:46 -0400

    initial environment files

---
+++ README.md
@@ -0,0 +1,4 @@
+# tesseract-olap client
+
+A javascript client to fetch and work with entities from a [tesseract-olap](https://www.tesseract-olap.io/) server.
+Heavily inspired by the [mondrian-rest-client](https://github.com/Datawheel/mondrian-rest-client) project, but with some added functionality.
---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import {terser} from "rollup-plugin-terser";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import replace from "rollup-plugin-replace";
+import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+
+import {
+  browser as browserModulePath,
+  module as esModulePath,
+  main as cjsModulePath
+} from "./package.json";
+
+const environment = process.env.NODE_ENV;
+const showSourceMaps = environment === "development";
+const sourcemap = showSourceMaps ? "inline" : false;
+
+export default commandLineArgs => {
+  return {
+    input: "src/index.ts",
+    output: [
+      {
+        file: esModulePath,
+        format: "esm",
+        sourcemap
+      },
+      {
+        file: cjsModulePath,
+        format: "cjs",
+        sourcemap
+      },
+      {
+        file: browserModulePath,
+        format: "iife",
+        name: "TesseractClient",
+        sourcemap
+      }
+    ],
+    plugins: [
+      replace({
+        ENVIRONMENT: JSON.stringify(environment)
+      }),
+      resolve(),
+      json(),
+      typescript(),
+      commonjs({
+        include: ["node_modules/**"],
+        namedExports: {}
+      }),
+      babel({
+        exclude: "node_modules/**"
+      }),
+      terser({keep_classnames: true})
+    ],
+    external: ["axios"],
+    watch: {
+      include: ["src/**"],
+      exclude: "node_modules/**",
+      clearScreen: true
+    }
+  };
+};
